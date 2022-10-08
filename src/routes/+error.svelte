a575ab7b8f99d02618bcc9dd41b39f6659d112fd
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-10-08 22:04:48 +0200

    init error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<!-- This page handles any error encountered by the site. -->
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>{$page.status}</h1>
+<p class="subhead">{$page.error.message}</p>
+
+<p><strong>Sorry!</strong> Maybe try one of these links?</p>
+<ul>
+	<li><a href="/">Home</a></li>
+</ul>
