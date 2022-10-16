88388e4aa87fb5fe2fd6669196f9388904125ab0
Sebastian Lammers <sebastian.lammers@posteo.de>
2022-10-16 21:54:08 +0200

    init error logo

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,31 @@
 <!-- This page handles any error encountered by the site. -->
 <script>
+	import { navItems } from '$lib/config';
 	import { page } from '$app/stores';
 </script>
 
+<img src="/error.png" alt="a logo showing a broken heart, the logo of Svelte and the logo of D3." />
 <h1>{$page.status}</h1>
 <p class="subhead">{$page.error.message}</p>
 
 <p><strong>Sorry!</strong> Maybe try one of these links?</p>
 <ul>
-	<li><a href="/">Home</a></li>
+	{#each navItems as page, i (i)}
+		<li>
+			<a href={page.route}>
+				{page.title}
+			</a>
+		</li>
+	{/each}
 </ul>
+
+<style>
+	img {
+		max-width: var(--space-3xl);
+		margin-bottom: 0%;
+	}
+
+	h1 {
+		margin: 0%;
+	}
+</style>
