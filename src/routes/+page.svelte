c7f410e8b3f03bbe5fd0ea848ca0ec3b2b4b12b0
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-02-05 20:30:42 +0100

    remove section container for consistency

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,31 +35,29 @@
 <h2>In the wild</h2>
 <p>Here are the three most recent projects:</p>
 
-<section class="u-container">
-	{#if $projectsQueryStore.fetching}
-		<p>Loading...</p>
-	{:else if $projectsQueryStore.error}
-		<p>Oopsie! {$projectsQueryStore.error.message}</p>
-	{:else}
-		<div class="u-grid">
-			{#each $projectsQueryStore.data.projects.slice(0, 3) as p}
-				<ProjectCard
-					name={p.name}
-					authors={p.authors}
-					description={p.description}
-					url={p.image[0].url}
-					slug={p.slug}
-					demo={p.demo}
-					sourceCode={p.sourceCode}
-					tags={p.tags}
-					{taglist}
-				/>
-			{/each}
-		</div>
+{#if $projectsQueryStore.fetching}
+	<p>Loading...</p>
+{:else if $projectsQueryStore.error}
+	<p>Oopsie! {$projectsQueryStore.error.message}</p>
+{:else}
+	<div class="u-grid">
+		{#each $projectsQueryStore.data.projects.slice(0, 3) as p}
+			<ProjectCard
+				name={p.name}
+				authors={p.authors}
+				description={p.description}
+				url={p.image[0].url}
+				slug={p.slug}
+				demo={p.demo}
+				sourceCode={p.sourceCode}
+				tags={p.tags}
+				{taglist}
+			/>
+		{/each}
+	</div>
 
-		<a class="button" href="/projects">Show all projects</a>
-	{/if}
-</section>
+	<a class="button" href="/projects">Show all projects</a>
+{/if}
 
 <h2>Tutorials</h2>
 <p>Here are the three most recent tutorials:</p>
