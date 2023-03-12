fcad5b3655d75f9c27e73450b95dcad61a0bbe1f
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-03-12 21:52:15 +0100

    init contact page with form

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Contact from '$lib/components/Contact.svelte';
+</script>
+
+<svelte:head>
+	<title>Awesome Svelte & D3 | Contact</title>
+</svelte:head>
+
+<h2>Contact</h2>
+
+<article class="flow">
+	<section>
+		<p>Do you want to suggest another project or tutorial to add to our collection?</p>
+		<p>Give us any kind of feedback? Use the form below!</p>
+	</section>
+
+	<Contact />
+</article>
