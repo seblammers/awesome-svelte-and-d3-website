b3a47579129abda75d2d786cf3b43ed868f8b318
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-04-23 11:36:55 +0200

    init sveltified logo

---
+++ src/lib/assets/svg/Logo.svelte
@@ -0,0 +1,182 @@
+<script>
+	export let fill = '#fcfcfc';
+	export let fillHeart = 'red';
+	export let stroke = '#1a1a1a';
+	export let strokeHeart = 'none';
+	export let width = '60';
+	$: height = width;
+</script>
+
+<svg
+	version="1.1"
+	{width}
+	{height}
+	viewBox="-10 -10 232 222"
+	id="svg266"
+	xml:space="preserve"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<defs id="defs270">
+		<clipPath id="clip-0">
+			<path
+				d="M0 0h7.8a45.5 45.5 0 1 1 0 91H0V71h7.8a25.5 25.5 0 1 0 0-51H0Zm36.3 0h32a27.8 27.8 0 0 1 21.3 45.5A27.8 27.8 0 0 1 68.3 91h-32A53.7 53.7 0 0 0 55 71h13.3a7.8 7.8 0 1 0 0-15.5h-7.8a53.7 53.7 0 0 0 0-20h7.8a7.8 7.8 0 1 0 0-15.5H55A53.7 53.7 0 0 0 36.3 0Z"
+				id="path240-9"
+			/>
+		</clipPath>
+		<clipPath clipPathUnits="userSpaceOnUse" id="clipPath1078">
+			<path
+				d="M0 0h7.8a45.5 45.5 0 1 1 0 91H0V71h7.8a25.5 25.5 0 1 0 0-51H0Zm36.3 0h32a27.8 27.8 0 0 1 21.3 45.5A27.8 27.8 0 0 1 68.3 91h-32A53.7 53.7 0 0 0 55 71h13.3a7.8 7.8 0 1 0 0-15.5h-7.8a53.7 53.7 0 0 0 0-20h7.8a7.8 7.8 0 1 0 0-15.5H55A53.7 53.7 0 0 0 36.3 0Z"
+				id="path1080"
+				stroke-width="1"
+			/>
+		</clipPath>
+		<clipPath clipPathUnits="userSpaceOnUse" id="clipPath1082">
+			<path
+				d="M0 0h7.8a45.5 45.5 0 1 1 0 91H0V71h7.8a25.5 25.5 0 1 0 0-51H0Zm36.3 0h32a27.8 27.8 0 0 1 21.3 45.5A27.8 27.8 0 0 1 68.3 91h-32A53.7 53.7 0 0 0 55 71h13.3a7.8 7.8 0 1 0 0-15.5h-7.8a53.7 53.7 0 0 0 0-20h7.8a7.8 7.8 0 1 0 0-15.5H55A53.7 53.7 0 0 0 36.3 0Z"
+				id="path1084"
+				stroke-width="1"
+			/>
+		</clipPath>
+		<clipPath clipPathUnits="userSpaceOnUse" id="clipPath1086">
+			<path
+				d="M0 0h7.8a45.5 45.5 0 1 1 0 91H0V71h7.8a25.5 25.5 0 1 0 0-51H0Zm36.3 0h32a27.8 27.8 0 0 1 21.3 45.5A27.8 27.8 0 0 1 68.3 91h-32A53.7 53.7 0 0 0 55 71h13.3a7.8 7.8 0 1 0 0-15.5h-7.8a53.7 53.7 0 0 0 0-20h7.8a7.8 7.8 0 1 0 0-15.5H55A53.7 53.7 0 0 0 36.3 0Z"
+				id="path1088"
+				stroke-width="1"
+			/>
+		</clipPath>
+		<clipPath id="clip-0-6">
+			<path
+				d="M0 0h7.8a45.5 45.5 0 1 1 0 91H0V71h7.8a25.5 25.5 0 1 0 0-51H0Zm36.3 0h32a27.8 27.8 0 0 1 21.3 45.5A27.8 27.8 0 0 1 68.3 91h-32A53.7 53.7 0 0 0 55 71h13.3a7.8 7.8 0 1 0 0-15.5h-7.8a53.7 53.7 0 0 0 0-20h7.8a7.8 7.8 0 1 0 0-15.5H55A53.7 53.7 0 0 0 36.3 0Z"
+				id="path240-9-5"
+			/>
+		</clipPath>
+		<linearGradient
+			id="linearGradient960"
+			gradientUnits="userSpaceOnUse"
+			x1="7"
+			y1="64"
+			x2="50"
+			y2="107"
+		>
+			<stop offset="0" stop-color="#f9a03c" id="stop243-2" />
+			<stop offset="1" stop-color="#f7974e" id="stop245-3" />
+		</linearGradient>
+		<linearGradient
+			id="linearGradient964"
+			gradientUnits="userSpaceOnUse"
+			x1="45"
+			y1="-10"
+			x2="108"
+			y2="53"
+		>
+			<stop offset="0" stop-color="#b84e51" id="stop253-6" />
+			<stop offset="1" stop-color="#f68e48" id="stop255-0" />
+		</linearGradient>
+		<linearGradient
+			id="linearGradient968"
+			gradientUnits="userSpaceOnUse"
+			x1="2"
+			y1="-2"
+			x2="87"
+			y2="84"
+		>
+			<stop offset="0" stop-color="#f26d58" id="stop248-2" />
+			<stop offset="1" stop-color="#f9a03c" id="stop250-6" />
+		</linearGradient>
+		<style type="text/css" id="style12223">
+		</style>
+		<g id="heart">
+			<path d="M0 200V0h200a100 100 90 0 1 0 200 100 100 90 0 1-200 0z" id="path12225" />
+		</g>
+	</defs>
+	<linearGradient id="gradient-1" gradientUnits="userSpaceOnUse" x1="7" y1="64" x2="50" y2="107">
+		<stop offset="0" stop-color="#f9a03c" id="stop243" />
+		<stop offset="1" stop-color="#f7974e" id="stop245" />
+	</linearGradient>
+	<linearGradient id="gradient-2" gradientUnits="userSpaceOnUse" x1="2" y1="-2" x2="87" y2="84">
+		<stop offset="0" stop-color="#f26d58" id="stop248" />
+		<stop offset="1" stop-color="#f9a03c" id="stop250" />
+	</linearGradient>
+	<linearGradient id="gradient-3" gradientUnits="userSpaceOnUse" x1="45" y1="-10" x2="108" y2="53">
+		<stop offset="0" stop-color="#b84e51" id="stop253" />
+		<stop offset="1" stop-color="#f68e48" id="stop255" />
+	</linearGradient>
+	<g
+		id="heartGroup"
+		transform="scale(-.6149) rotate(45 298.2 -367.3)"
+		fill-opacity="1"
+		stroke-width="17.7"
+		stroke-dasharray="none"
+		stroke-opacity="1"
+	>
+		<path
+			d="M0 200V0h200a100 100 90 0 1 0 200 100 100 90 0 1-200 0z"
+			id="heart"
+			fill={fillHeart}
+			fill-opacity="1"
+			stroke={strokeHeart}
+			stroke-width="17.7"
+			stroke-dasharray="none"
+			stroke-opacity="1"
+		/>
+	</g>
+	<path
+		id="rect308-6-2"
+		{fill}
+		fill-opacity="0"
+		stroke="none"
+		stroke-width="7.1"
+		stroke-linejoin="round"
+		stroke-miterlimit="10"
+		paint-order="fill markers stroke"
+		stop-color="#000"
+		d="M-320.1-359.2h298.6v298.6h-298.6z"
+	/>
+	<path
+		id="rect308"
+		{fill}
+		stroke="none"
+		stroke-width="5.5"
+		stroke-linejoin="round"
+		stroke-miterlimit="10"
+		paint-order="fill markers stroke"
+		stop-color="#000"
+		fill-opacity="0"
+		d="M-10-15h232v232H-10z"
+	/>
+	<g id="svelte-logo">
+		<g id="g1">
+			<path
+				class="st0"
+				d="M87 60a34 34 0 0 0-45.4-9.7l-26 16.6c-7 4.4-11.9 11.7-13.3 19.9-1.2 6.8-.2 14 3.1 20a31.7 31.7 0 0 0 1 35 34 34 0 0 0 45.4 10l26-16.6c7-4.4 11.9-11.7 13.3-19.9 1.2-6.9.2-14-3.1-20A31 31 0 0 0 87 60"
+				id="path4-8"
+				fill={stroke}
+				fill-opacity="1"
+				stroke-width=".9"
+			/>
+			<path
+				class="st1"
+				d="M39 143.3a20.5 20.5 0 0 1-22-8.2 19 19 0 0 1-3.3-14.4l.6-2.4.4-1.6 1.3 1c3.2 2.2 6.6 4 10.2 5l1 .4-.1.9c-.1 1.3.3 2.7 1 3.9a6.2 6.2 0 0 0 8.3 1.9l25.8-16.5c1.4-.9 2.2-2.1 2.5-3.6.3-1.5-.1-3.1-1-4.4a6.2 6.2 0 0 0-6.6-2.4l-1.6.6-10 6.4a20.5 20.5 0 0 1-27.3-6A19 19 0 0 1 15 89.5c.9-4.9 3.9-9.3 8.1-12l26-16.5c1.5-1 3.3-1.8 5.2-2.3a20.5 20.5 0 0 1 22 8.2A19 19 0 0 1 79 83.7l-.4 1.6-1.4-1c-3-2.2-6.5-4-10.1-5l-1-.4.1-.9c.1-1.3-.3-2.7-1-3.9a6.2 6.2 0 0 0-8.3-1.8L31 88.8c-1.3.9-2.1 2.1-2.4 3.6-.3 1.5 0 3.1 1 4.4a6.2 6.2 0 0 0 8.2 1.7l10-6.3c1.5-1 3.3-1.8 5.2-2.3A20.5 20.5 0 0 1 75 98a19 19 0 0 1 3.4 14.4c-.9 4.9-3.9 9.3-8.1 12l-26 16.5c-1.6 1-3.4 1.8-5.2 2.3"
+				id="path6-2"
+				{fill}
+				stroke-width=".9"
+			/>
+		</g>
+		<g id="g2">
+			<path
+				id="use1-5"
+				d="M112.5 58.7h7.5a44.1 44.1 0 1 1 0 88.2h-7.5v-19.4h7.5a24.7 24.7 0 1 0 0-49.4h-7.5zm35.1 0h31a27 27 0 0 1 20.7 44.1 27 27 0 0 1-20.6 44.1h-31a52 52 0 0 0 18.1-19.4h12.9a7.5 7.5 0 1 0 0-15H171a52 52 0 0 0 0-19.4h7.6a7.5 7.5 0 1 0 0-15h-12.9a52 52 0 0 0-18.2-19.4Z"
+				fill="none"
+				{stroke}
+				stroke-width="19.4"
+				stroke-opacity="1"
+			/>
+			<path
+				id="use2"
+				d="M112.5 58.7h7.5a44.1 44.1 0 1 1 0 88.2h-7.5v-19.4h7.5a24.7 24.7 0 1 0 0-49.4h-7.5zm35.1 0h31a27 27 0 0 1 20.7 44.1 27 27 0 0 1-20.6 44.1h-31a52 52 0 0 0 18.1-19.4h12.9a7.5 7.5 0 1 0 0-15H171a52 52 0 0 0 0-19.4h7.6a7.5 7.5 0 1 0 0-15h-12.9a52 52 0 0 0-18.2-19.4Z"
+				{fill}
+				stroke-width="1"
+			/>
+		</g>
+	</g>
+</svg>
