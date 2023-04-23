97bd1a369b8274fd84af63873b31cbe00a26d73d
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-04-23 11:43:18 +0200

    add meta in head

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,4 +3,8 @@
 	export let data;
 </script>
 
+<svelte:head>
+	<title>Awesome Svelte & D3 | About</title>
+</svelte:head>
+
 <h2>About this site</h2>
--- src/routes/learning/+page.svelte
+++ src/routes/learning/+page.svelte
@@ -3,4 +3,8 @@
 	export let data;
 </script>
 
+<svelte:head>
+	<title>Awesome Svelte & D3 | Learning</title>
+</svelte:head>
+
 <h2>Learning resources</h2>
