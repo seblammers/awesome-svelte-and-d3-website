e736d86581cfe9528f19fe8351f4b7c765958b5a
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-04-23 21:39:40 +0200

    update meta (redundancy)

--- src/lib/components/Meta.svelte
+++ src/lib/components/Meta.svelte
@@ -3,8 +3,7 @@
 	import image from '$lib/assets/images/og-image.png';
 
 	let title = 'Awesome Svelte & D3';
-	let description =
-		'Awesome Svelte & D3 is a website to learn about great data visualization projects and underlying techniques.';
+	let description = 'Learn about great data visualization projects and underlying techniques.';
 	let url = 'https://awesome-svelte-and-d3.netlify.app';
 	let authorName = 'Sebastian Lammers et al.';
 	let website = url;
