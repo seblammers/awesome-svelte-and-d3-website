41ef8075d947846aa08fc5cf6fed9115deb058ad
Sebastian Lammers <sebastian.lammers@posteo.de>
2023-04-23 21:28:24 +0200

    init meta-component

---
+++ src/lib/components/Meta.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Head } from 'svead';
+	import image from '$lib/assets/images/og-image.png';
+
+	let title = 'Awesome Svelte & D3';
+	let description =
+		'Awesome Svelte & D3 is a website to learn about great data visualization projects and underlying techniques.';
+	let url = 'https://awesome-svelte-and-d3.netlify.app';
+	let authorName = 'Sebastian Lammers et al.';
+	let website = url;
+</script>
+
+<Head {title} {description} {image} {url} {authorName} {website} />
